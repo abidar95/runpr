d0a94d76f4c51c8f4ffc280301e93b1ed9769b89
TED <rey@new-host-2.home>
2017-09-17 21:49:14 +0100

    update setup files

--- setup.py
+++ setup.py
@@ -5,6 +5,13 @@ setup(
     version="1.0",
     py_modules=["runpr"],
     install_requires=["Click"],
+    description = 'CLI for running Python Project with Virtualenv via Visual Studio Code',
+    author = 'Abidar El Mehdi',
+    author_email = 'abidar.mehdi95@gmail.com',
+    url = 'https://github.com/peterldowns/mypackage',
+    download_url = 'https://github.com/abidar95/runpr', 
+    keywords = ['running', 'managing'],
+    classifiers = [],
     entry_points="""
         [console_scripts]
         runpr=runpr:run
